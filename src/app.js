464ba48b515997666de696841799889fff370955
Patrick Njoli <patimjoli@gmail.com>
2021-01-21 09:19:30 +0100

    Updated some text

--- src/app.js
+++ src/app.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Welcome to Cerebrum</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://cerebrumhq.herokuapp.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
