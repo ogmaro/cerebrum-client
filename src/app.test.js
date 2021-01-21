4263a5e7acaec6794c7983283b5a52af1453e9c8
Patrick Njoli <patimjoli@gmail.com>
2021-01-21 09:24:23 +0100

    fixed problem with test

--- src/app.test.js
+++ src/app.test.js
@@ -3,6 +3,6 @@ import App from "./app";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome to Cerebrum/i);
   expect(linkElement).toBeInTheDocument();
 });
