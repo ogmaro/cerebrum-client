3f69f068e480a3c8b1521b8deaa218659b80c30c
emmysoft123 <47819331+emmysoft123@users.noreply.github.com>
2021-02-08 14:19:13 -0800

    made some updates to the pages and component folder respectively

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React ,{Component,useState }from 'react';
+import React from 'react';
 import {BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 //import Navbar from './components/Navbar';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//<Route path="/Login" component={Dashboard_NavbarPage} exact />
 import SigninPage from './pages/signin';
-import LoginPage from './pages/Login';
- //  <Route path="/Login" component={Dashboard_navbarPage} exact />
 
-import Dashboard_navbarPage from './pages/Dashboard';
+import LoginPage from './pages/Login';
+
+ 
+//import Dashboard_NavbarPage from './pages/Dashboard';
 import Home from './pages';
-import Homes from './pages/Homes';
-import Footer from './components/Footer';
+
+//import Footer from './components/Footer';
 //import Sidebar from './components/Sidebar';
 function App() {
 	
@@ -20,9 +22,10 @@ function App() {
     <Switch>
      <Route path="/" component= {Home} exact />
     <Route path="/signin" component={SigninPage} exact />
-    <Route path="/Login" component={LoginPage} exact />
+    <Route path="/Login" component={LoginPage} exact />    
+
   
-        <Route path="/" component={Homes} exact />
+      
     </Switch>
     
    
